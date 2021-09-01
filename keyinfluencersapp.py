cbff52f9cff47932420e1feaaaf7ada9909ad8a9
Roger Pou <ropolodos@gmail.com>
2021-09-01 13:00:05 +0200

    Update keyinfluencersapp.py

--- keyinfluencersapp.py
+++ keyinfluencersapp.py
@@ -28,92 +28,87 @@ if __name__ == "__main__":
     if not input:
         st.sidebar.subheader("Or you can directly load a Complete Example")
         with st.sidebar.expander("Examples"):
-<<<<<<< HEAD
+
             check1=st.checkbox("Ex 1. Price column in Automobile")
             check2=st.checkbox("Ex 2. Price in Diamond")
             check3=st.checkbox("Ex 3. Strength in Concrete")
             check4=st.checkbox("Ex 4. CNT in Bike")
             if check1+check2+check3+check4>1:
                 st.sidebar.warning("Check only one example at time")
-=======
-            check1=st.sidebar.checkbox("Ex 1. Price column in Automobile")
-            check2=st.sidebar.checkbox("Ex 2. Price in Diamond")
-            check3=st.sidebar.checkbox("Ex 3. Strength in Concrete")
-            check4=st.sidebar.checkbox("Ex 4. cnt in Bike")
->>>>>>> parent of cbac5e3 (Update keyinfluencersapp.py)
 
-            if check1:
-                from pycaret.datasets import get_data
-                df = get_data('automobile')
+
+        if check1:
+            from pycaret.datasets import get_data
+            df = get_data('automobile')
+            st.dataframe(df.head(10))
+            with st.spinner("Analyzing..."):
+                reg = pr.setup(
+                    df,
+                    target='price',
+                    use_gpu=True,
+                    silent=True,
+                    feature_selection=True
+                )
+
+                lgbm = pr.create_model('lightgbm')
+                fig = pr.interpret_model(lgbm)
+                st.pyplot(fig)
+                st.sidebar.success("Succesful Analysis")
+        if check2:
+            from pycaret.datasets import get_data
+
+            df = get_data('diamond')
+            st.dataframe(df.head(10))
+            with st.spinner("Analyzing..."):
+                reg = pr.setup(
+                    df,
+                    target='price',
+                    use_gpu=True,
+                    silent=True,
+                    feature_selection=True
+                )
+
+                lgbm = pr.create_model('lightgbm')
+                fig = pr.interpret_model(lgbm)
+                st.pyplot(fig)
+                st.sidebar.success("Succesful Analysis")
+        if check3:
+            from pycaret.datasets import get_data
+
+            df = get_data('concrete')
+            with st.expander('Explore data'):
                 st.dataframe(df.head(10))
-                with st.spinner("Analyzing..."):
-                    reg = pr.setup(
-                        df,
-                        target='price',
-                        use_gpu=True,
-                        silent=True,
-                        feature_selection=True
-                    )
+            with st.spinner("Analyzing..."):
+                reg = pr.setup(
+                    df,
+                    target='strength',
+                    use_gpu=True,
+                    silent=True,
+                    feature_selection=True
+                )
 
-                    lgbm = pr.create_model('lightgbm')
-                    fig = pr.interpret_model(lgbm)
-                    st.pyplot(fig)
-                    st.sidebar.success("Succesful Analysis")
-                if check2:
-                    from pycaret.datasets import get_data
+                lgbm = pr.create_model('lightgbm')
+                fig = pr.interpret_model(lgbm)
+                st.pyplot(fig)
+                st.sidebar.success("Succesful Analysis")
+        if check4:
+            from pycaret.datasets import get_data
 
-                    df = get_data('diamond')
-                    st.dataframe(df.head(10))
-                    with st.spinner("Analyzing..."):
-                        reg = pr.setup(
-                            df,
-                            target='price',
-                            use_gpu=True,
-                            silent=True,
-                            feature_selection=True
-                        )
+            df = get_data('concrete')
+            st.dataframe(df.head(10))
+            with st.spinner("Analyzing..."):
+                reg = pr.setup(
+                    df,
+                    target='cnt',
+                    use_gpu=True,
+                    silent=True,
+                    feature_selection=True
+                )
 
-                        lgbm = pr.create_model('lightgbm')
-                        fig = pr.interpret_model(lgbm)
-                        st.pyplot(fig)
-                        st.sidebar.success("Succesful Analysis")
-                    if check3:
-                        from pycaret.datasets import get_data
-
-                        df = get_data('concrete')
-                        with st.expander('Explore data'):
-                            st.dataframe(df.head(10))
-                        with st.spinner("Analyzing..."):
-                            reg = pr.setup(
-                                df,
-                                target='strength',
-                                use_gpu=True,
-                                silent=True,
-                                feature_selection=True
-                            )
-
-                            lgbm = pr.create_model('lightgbm')
-                            fig = pr.interpret_model(lgbm)
-                            st.pyplot(fig)
-                            st.sidebar.success("Succesful Analysis")
-                    if check4:
-                        from pycaret.datasets import get_data
-
-                        df = get_data('concrete')
-                        st.dataframe(df.head(10))
-                        with st.spinner("Analyzing..."):
-                            reg = pr.setup(
-                                df,
-                                target='cnt',
-                                use_gpu=True,
-                                silent=True,
-                                feature_selection=True
-                            )
-
-                            lgbm = pr.create_model('lightgbm')
-                            fig = pr.interpret_model(lgbm)
-                            st.pyplot(fig)
-                            st.sidebar.success("Succesful Analysis")
+                lgbm = pr.create_model('lightgbm')
+                fig = pr.interpret_model(lgbm)
+                st.pyplot(fig)
+                st.sidebar.success("Succesful Analysis")
     if input:
 
 
@@ -140,10 +135,10 @@ if __name__ == "__main__":
                 st.pyplot(fig)
                 st.sidebar.success("Succesful Analysis")
     st.sidebar.header('About')
-    st.sidebar.success(
+    st.sidebar.warning(
         """
            Python Key Influencers app is maintained by 
-           **Roger Pou López** while working as a Data Scientist in Grupo Vall Companys. If you like this app please star its
+           **Roger Pou López** while working as Data Scientist in Grupo Vall Companys. If you like this app please star its
            [**GitHub**](https://github.com/rogerpou/Key-Influencers-App)
            repo, share it and feel free to open an issue if you find a bug 
            or if you want some additional features. 
